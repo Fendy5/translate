8d37a9e16d7d09b8534810fca6e4ba165882baa7
Fendy <zf@fendy5.cn>
2024-01-04 22:10:04 +0800

    build: 路径别名

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-};
+  plugins: [
+    // 配置 babel-plugin-module-resolver
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        root: ['.src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@': ['./src'],
+          '~images': './src/assets/images',
+          '~components': './src/components',
+          '~utils': './src/utils'
+        }
+      }
+    ]
+  ]
+}
---
+++ src/index.d.ts
@@ -0,0 +1,7 @@
+/**
+ * @Author fendy
+ * @CreateTime 2024/1/3 22:40
+ * @Description
+ */
+declare module '*.png'
+declare module '*.svg'
